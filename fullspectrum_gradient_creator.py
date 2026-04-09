209605695e21cf60ed6d1242a4f37c5152162dcb
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2026-04-09 07:37:13 +0200

    Update fullspectrum_gradient_creator.py
    added offset for gradient start
    switched from fixed gradient steps to userspecified stepsize

--- fullspectrum_gradient_creator.py
+++ fullspectrum_gradient_creator.py
@@ -1,7 +1,17 @@
-gradientstart = int(input('enter first color value (int):'))
-gradientend = int(input('enter second color value (int):'))
-size = int(input('enter size (layers) of gradient:'))
+gradientstart = int(input('enter first color value (int): '))
+gradientend = int(input('enter second color value (int): '))
+size = int(input('enter size/layers of gradient (int): '))
+gradientoffset = int(input('enter offset in layers from the bottom (int): '))
+smallgradient = int(input('enter gadient stepsize (int): '))
 
+def getratioarray(stepsize) -> list:
+    array = []
+    step = 1
+    while step > 0:
+        step -= (stepsize/100)
+        array.append(step)
+    return array
+    
 def generatepattern(a: int, b: int, ratioa: float, length: int) -> str:
     result = []
     error = 1.0
@@ -16,16 +26,20 @@ def generatepattern(a: int, b: int, ratioa: float, length: int) -> str:
 
     return "".join(result)
     
-def generategradient(a: int, b: int, size: int) -> str:
-    ratios = [0.96,0.95,0.91,0.9,0.87,0.85,0.83,0.8,0.79,0.75,0.7,0.66,0.65,0.62,0.6,0.58,0.54,0.5,0.46,0.42,0.4,0.38,0.35,0.34,0.3,0.25,0.21,0.2,0.17,0.15,0.13,0.1,0.09,0.05,0.04]
+def generategradient(a: int, b: int, size: int, offset: int, ratiostepsize: int) -> str:
+    #ratios = [0.96,0.95,0.91,0.9,0.87,0.85,0.83,0.8,0.79,0.75,0.7,0.66,0.65,0.62,0.6,0.58,0.54,0.5,0.46,0.42,0.4,0.38,0.35,0.34,0.3,0.25,0.21,0.2,0.17,0.15,0.13,0.1,0.09,0.05,0.04]
+    ratios = getratioarray(ratiostepsize)
     patterns = []
     patternsize = size // len(ratios)
+    
+    for _ in range(offset):
+        patterns.append(str(a))
     
     for ratio in ratios:
         patterns.append(generatepattern(a, b, ratio, patternsize))
     
     return "".join(patterns)
     
-print(generategradient(gradientstart,gradientend,size))
+print(generategradient(gradientstart,gradientend,size,gradientoffset, smallgradient))
 
 input()
